6b729e5e08d756828169e1077f83c14397a9e174
schikimikiviki <viktoria.bzdyk@gmx.at>
2023-10-06 19:12:50 +0200

    dogstorage created

--- src/main/java/com/robodog/dog/model/Dog.java
+++ src/main/java/com/robodog/dog/model/Dog.java
@@ -14,4 +14,30 @@ public class Dog {
         this.breed = breed;
 
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Breed getBreed() {
+        return breed;
+    }
+
+    public void setBreed(Breed breed) {
+        this.breed = breed;
+    }
+
+
 }
--- src/main/java/com/robodog/dog/service/DogStorage.java
+++ src/main/java/com/robodog/dog/service/DogStorage.java
@@ -1,4 +1,41 @@
 package com.robodog.dog.service;
 
+import com.robodog.dog.model.Breed;
+import com.robodog.dog.model.Dog;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 public class DogStorage {
+
+    private List<Dog> listOfDogs = new ArrayList<>();
+
+    public void addDog(Dog dog) {
+        listOfDogs.add(dog);
+    }
+
+    public void addRandomDog() {
+        DogCreator dogCreator = new DogCreator();
+        listOfDogs.add(dogCreator.createRandomDog());
+    }
+
+    public List<Dog> getAllDogs() {
+        return listOfDogs;
+    }
+
+    public void updateAgeAndBreed(String name, int newAge, Breed breed) {
+        Optional<Dog> dogWithGivenName = listOfDogs.stream()
+                .filter(dog -> dog.getName().equals(name))
+                .findFirst();
+
+        if (dogWithGivenName.isPresent()) {
+            Dog dog = dogWithGivenName.get();
+            dog.setAge(newAge);
+            dog.setBreed(breed);
+            System.out.println("Dog with given name updated!");
+        } else {
+            System.out.println("Dog with given name not found.");
+        }
+    }
 }
